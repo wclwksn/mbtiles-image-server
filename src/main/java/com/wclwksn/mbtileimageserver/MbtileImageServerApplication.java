6c71749480fcd9ed472ff663db5c6ddca1931d2e
www <wcl@sina.com>
2020-04-01 16:17:30 +0800

    删除无用代码

--- src/main/java/com/wclwksn/mbtileimageserver/MbtileImageServerApplication.java
+++ src/main/java/com/wclwksn/mbtileimageserver/MbtileImageServerApplication.java
@@ -6,71 +6,13 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import com.wclwksn.mbtileimageserver.repository.mbconfigRespository;
+import org.springframework.context.annotation.Bean; 
 
 @SpringBootApplication
-public class MbtileImageServerApplication {
-
-	private static final Logger log = LoggerFactory.getLogger(MbtileImageServerApplication.class);
+public class MbtileImageServerApplication { 
 
 	public static void main(String[] args) {
+		
 		SpringApplication.run(MbtileImageServerApplication.class, args);
 	}
-
-	
-
-	@Bean
-	public CommandLineRunner getAllData(mbconfigRespository repository) {
-		return (args) -> {
-			 
-		};
-	}
-
-	
-
-//	public List<File> getallmbFiles() {
-//		log.info("执行一次");
-//		
-//		return readAllFile(mbpath);
-//	}
-	
-//	  @Bean
-//	  public CommandLineRunner demo(CustomerRepository repository) {
-//	    return (args) -> {
-//	      // save a few customers
-//	      repository.save(new Customer("Jack", "Bauer"));
-//	      repository.save(new Customer("Chloe", "O'Brian"));
-//	      repository.save(new Customer("Kim", "Bauer"));
-//	      repository.save(new Customer("David", "Palmer"));
-//	      repository.save(new Customer("Michelle", "Dessler"));
-//
-//	      // fetch all customers
-//	      log.info("Customers found with findAll():");
-//	      log.info("-------------------------------");
-//	      for (Customer customer : repository.findAll()) {
-//	        log.info(customer.toString());
-//	      }
-//	      log.info("");
-//
-//	      // fetch an individual customer by ID
-//	      Customer customer = repository.findById(1L);
-//	      log.info("Customer found with findById(1L):");
-//	      log.info("--------------------------------");
-//	      log.info(customer.toString());
-//	      log.info("");
-//
-//	      // fetch customers by last name
-//	      log.info("Customer found with findByLastName('Bauer'):");
-//	      log.info("--------------------------------------------");
-//	      repository.findByLastName("Bauer").forEach(bauer -> {
-//	        log.info(bauer.toString());
-//	      });
-//	      // for (Customer bauer : repository.findByLastName("Bauer")) {
-//	      //  log.info(bauer.toString());
-//	      // }
-//	      log.info("");
-//	    };
-//	  }
 }
